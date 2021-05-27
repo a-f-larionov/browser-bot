82a17f2ec31f8240d3fc595470909a2fefca07e5
a.f.larionov <a.f.larionov@gmail.com>
2021-05-27 17:36:41 +0300

    some changes before testings

--- src/test/java/taplinkbot/ApplicationTests.java
+++ src/test/java/taplinkbot/ApplicationTests.java
@@ -2,12 +2,19 @@ package taplinkbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.env.MockEnvironment;
 
 @SpringBootTest
 class ApplicationTests {
 
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    void test() {
+        MockEnvironment mockEnvironment;
 
     }
 }
