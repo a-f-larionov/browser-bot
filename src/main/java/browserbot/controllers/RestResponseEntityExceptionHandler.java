3218f17154ad0641a577047da471120d709d63a4
a.f.larionov <a.f.larionov@gmail.com>
2021-06-03 20:22:04 +0300

    REST REponse ENtiity Exception

--- src/main/java/browserbot/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/browserbot/controllers/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,10 @@
+//FIN
 package browserbot.controllers;
 
+import browserbot.services.LangService;
+import browserbot.telegram.TelegramBot;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,15 +12,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@todo
 @ControllerAdvice
+@RequiredArgsConstructor
+@Slf4j
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final LangService lang;
+
+    private final TelegramBot telegramBot;
 
     @ExceptionHandler
     public ResponseEntity<Object> handleConflict(Exception e, WebRequest request) {
 
+        telegramBot.alert(lang.get("application.alert_controller_exception"));
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
+                .body(lang.get("application.exception_handled"));
     }
 }
